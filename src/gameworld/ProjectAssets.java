3cdd54c901afa62bb54228a9c3957405daa9688c
Niko <ngeorgiev768@gmail.com>
2026-02-23 11:47:05 +0100

    Liamoiazo de Assets

--- src/gameworld/ProjectAssets.java
+++ src/gameworld/ProjectAssets.java
@@ -82,45 +82,7 @@ public final class ProjectAssets {
 
         // region backgrounds (bg-***)
         // NOTA: el fondo del tema activo se registra como "theme_back" en registerThemeAssets()
-        this.catalog.register("back_01", "bg-01-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_02", "bg-02-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_03", "bg-03-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_04", "bg-04-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_05", "bg-05-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_06", "bg-06-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_07", "bg-07-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_08", "bg-08-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_09", "bg-09-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_10", "bg-10-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_11", "bg-11-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_12", "bg-12-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_13", "bg-13-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_14", "bg-14-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_15", "bg-15-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_16", "bg-16-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_17", "bg-17-space.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_18", "bg-18-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_19", "bg-19-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_20", "bg-20-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_21", "bg-21-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_22", "bg-22-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_23", "bg-23-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_24", "bg-24-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        this.catalog.register("back_25", "bg-25-space-seamless.jpg", AssetType.BACKGROUND, AssetIntensity.LOW);
-        // endregion
-
-        // region black holes (black-hole-***)
-        this.catalog.register("black_hole_01", "black-hole-01.png", AssetType.BLACK_HOLE, AssetIntensity.HIGH);
-        this.catalog.register("black_hole_02", "black-hole-02.png", AssetType.BLACK_HOLE, AssetIntensity.HIGH);
-        // endregion
-
-        // region bubbles (bubbles-***)
-        this.catalog.register("bubbles_01", "bubbles-01-mini.png", AssetType.BUBBLES, AssetIntensity.HIGH);
-        this.catalog.register("bubbles_02", "bubbles-02-mini.png", AssetType.BUBBLES, AssetIntensity.HIGH);
-        this.catalog.register("bubbles_03", "bubbles-03-mini.png", AssetType.BUBBLES, AssetIntensity.HIGH);
-        this.catalog.register("bubbles_04", "bubbles-04-mini.png", AssetType.BUBBLES, AssetIntensity.HIGH);
-        // endregion
-
+        
         // region bullets (bullet-***)
         this.catalog.register("bullet_01", "bullet-01.png", AssetType.BULLET, AssetIntensity.LOW);
         // endregion
@@ -138,46 +100,6 @@ public final class ProjectAssets {
 
         // region cosmic portals (cosmic-portal-***)
         this.catalog.register("cosmic_portal_01", "cosmic-portal-01.png", AssetType.COSMIC_PORTAL, AssetIntensity.HIGH);
-        // endregion
-
-        // region cracks (cracks-***)
-        this.catalog.register("cracks_01", "cracks-01.png", AssetType.CRACKS, AssetIntensity.HIGH);
-        this.catalog.register("cracks_02", "cracks-02.png", AssetType.CRACKS, AssetIntensity.HIGH);
-        this.catalog.register("cracks_03", "cracks-03.png", AssetType.CRACKS, AssetIntensity.HIGH);
-        this.catalog.register("cracks_04", "cracks-04.png", AssetType.CRACKS, AssetIntensity.HIGH);
-        this.catalog.register("cracks_05", "cracks-05.png", AssetType.CRACKS, AssetIntensity.HIGH);
-        // endregion
-
-        // region galaxies (galaxy-***)
-        this.catalog.register("galaxy_01", "galaxy-01.png", AssetType.GALAXY, AssetIntensity.HIGH);
-        this.catalog.register("galaxy_02", "galaxy-02.png", AssetType.GALAXY, AssetIntensity.HIGH);
-        this.catalog.register("galaxy_03", "galaxy-03.png", AssetType.GALAXY, AssetIntensity.HIGH);
-        this.catalog.register("galaxy_03", "galaxy-04.png", AssetType.GALAXY, AssetIntensity.HIGH);
-        this.catalog.register("galaxy_04", "galaxy-05.png", AssetType.GALAXY, AssetIntensity.HIGH);
-        this.catalog.register("galaxy_06", "galaxy-06.png", AssetType.GALAXY, AssetIntensity.HIGH);
-        // endregion
-
-        // region halos (halo-***))
-        this.catalog.register("halo_01", "halo-01.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_02", "halo-02.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_03", "halo-03.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_04", "halo-04.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_05", "halo-05.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_06", "halo-06.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_07", "halo-07.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_08", "halo-08.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_09", "halo-09.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_10", "halo-10.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_11", "halo-11.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_12", "halo-12.png", AssetType.HALO, AssetIntensity.HIGH);
-        this.catalog.register("halo_13", "halo-13.png", AssetType.HALO, AssetIntensity.HIGH);
-        // endregion
-
-        // region labs (lab-***)
-        this.catalog.register("lab_01", "lab-01.png", AssetType.LAB, AssetIntensity.HIGH);
-        this.catalog.register("lab_02", "lab-02.png", AssetType.LAB, AssetIntensity.HIGH);
-        this.catalog.register("lab_03", "lab-03.png", AssetType.LAB, AssetIntensity.HIGH);
-        this.catalog.register("lab_04", "lab-04.png", AssetType.LAB, AssetIntensity.HIGH);
         // endregion
 
         // region lights (light-***)
@@ -216,40 +138,6 @@ public final class ProjectAssets {
         this.catalog.register("rocket_06", "rocket-06.png", AssetType.ROCKET, AssetIntensity.HIGH);
         this.catalog.register("rocket_07", "rocket-07.png", AssetType.ROCKET, AssetIntensity.HIGH);
         // endregion
-
-        // region shot holes (shot-hole-***)
-        this.catalog.register("shot_hole_01", "shot-hole-01.png", AssetType.SHOT_HOLE, AssetIntensity.HIGH);
-        this.catalog.register("shot_hole_02", "shot-hole-02.png", AssetType.SHOT_HOLE, AssetIntensity.HIGH);
-        this.catalog.register("shot_hole_03", "shot-hole-03.png", AssetType.SHOT_HOLE, AssetIntensity.HIGH);
-        // endregion
-
-        // region stars (stars-***)
-       
-        // endregion
-
-        // region stardusts (stardust-***)
-        this.catalog.register("stardust_01", "stardust-01.png", AssetType.STARDUST, AssetIntensity.HIGH);
-        this.catalog.register("stardust_02", "stardust-02.png", AssetType.STARDUST, AssetIntensity.HIGH);
-
-        // region spaceships (spaceship-***)
-        this.catalog.register("spaceship_01", "pac-man.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_02", "spaceship-02.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_03", "spaceship-03.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_04", "spaceship-04.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_05", "spaceship-05.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_06", "spaceship-06.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_07", "spaceship-07.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_08", "spaceship-08.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_09", "spaceship-09.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_10", "spaceship-10.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_11", "spaceship-11.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_12", "spaceship-12.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_13", "spaceship-13.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_14", "spaceship-14.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("spaceship_15", "spaceship-15.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("pac-man-a-1", "pac-man-a-1.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("pac-man-a-2", "pac-man-a-2.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
-        this.catalog.register("pac-man-a-3", "pac-man-a-3.png", AssetType.SPACESHIP, AssetIntensity.HIGH);
 
         // Player weapon visual overlay - AssetType.WEAPON solo para armas visuales
         this.catalog.register("player_weapon", "ak-47.png", AssetType.WEAPON, AssetIntensity.LOW);
